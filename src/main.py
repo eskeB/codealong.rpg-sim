70ac9c565bf5a407002a316a39e2435611e90796
Eskil <eskil.erik@gmail.com>
2022-02-11 12:56:27 +0100

    Added character class

---
+++ src/main.py
@@ -0,0 +1,15 @@
+from resources import Character
+
+def main(): #hp, damage, armor
+    Linus = Character("Lelle", 15, 6, 5)
+    Adam = Character("Adam", 3, 1, 1)
+    Pelle = Character("Pelle", 5, 20, 1)
+    Erik = Character("Erik", 15, 6, 5)
+    
+    print(Linus)
+    print(Adam)
+    print(Pelle)
+    print(Erik)
+
+if __name__ == "__main__":
+    main()
---
+++ src/resources.py
@@ -0,0 +1,30 @@
+
+#imports
+#global variables
+#classes
+
+
+
+from tkinter import N
+
+
+class Character:
+    
+    def __init__(self, name, health, damage, armor):
+        self.name = name
+        self.health = health
+        self.damage = damage
+        self.armor = armor
+
+    def __str__(self):
+        return f'Name:{self.name}\nHealth:{self.health}\nDamage : {self.damage}\n Armor : {self.armor}\n'
+#functions
+
+def hello():
+    print("Hello world")
+#main code
+
+
+
+
+
